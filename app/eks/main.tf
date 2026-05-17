235e23662c2794c32c077f6ccedf0231d827317c
niteshpotu5656 <niteshpotu5656@gmail.com>
2026-05-17 12:34:06 +0530

    feat: add app/eks/main.tf

---
+++ app/eks/main.tf
@@ -0,0 +1,67 @@
+resource "aws_iam_role" "cluster" {{
+  name = "${{var.cluster_name}}-cluster-role"
+  assume_role_policy = jsonencode({{
+    Version = "2012-10-17"
+    Statement = [{{
+      Effect    = "Allow"
+      Principal = {{ Service = "eks.amazonaws.com" }}
+      Action    = "sts:AssumeRole"
+    }}]
+  }})
+  tags = var.tags
+}}
+
+resource "aws_iam_role_policy_attachment" "cluster_policy" {{
+  role       = aws_iam_role.cluster.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+}}
+
+resource "aws_eks_cluster" "main" {{
+  name     = var.cluster_name
+  version  = var.k8s_version
+  role_arn = aws_iam_role.cluster.arn
+
+  vpc_config {{
+    subnet_ids              = var.subnet_ids
+    security_group_ids      = [var.sg_id]
+    endpoint_private_access = true
+    endpoint_public_access  = false
+  }}
+
+  encryption_config {{
+    resources = ["secrets"]
+    provider {{ key_arn = var.kms_key_arn }}
+  }}
+
+  tags = merge(var.tags, {{ Name = var.cluster_name }})
+  depends_on = [aws_iam_role_policy_attachment.cluster_policy]
+}}
+
+resource "aws_iam_role" "node" {{
+  name = "${{var.cluster_name}}-node-role"
+  assume_role_policy = jsonencode({{
+    Version = "2012-10-17"
+    Statement = [{{
+      Effect    = "Allow"
+      Principal = {{ Service = "ec2.amazonaws.com" }}
+      Action    = "sts:AssumeRole"
+    }}]
+  }})
+  tags = var.tags
+}}
+
+resource "aws_eks_node_group" "main" {{
+  cluster_name    = aws_eks_cluster.main.name
+  node_group_name = "${{var.cluster_name}}-node-group"
+  node_role_arn   = aws_iam_role.node.arn
+  subnet_ids      = var.subnet_ids
+  instance_types  = [var.node_instance_type]
+
+  scaling_config {{
+    desired_size = var.node_desired
+    min_size     = var.node_min
+    max_size     = var.node_max
+  }}
+
+  tags = merge(var.tags, {{ Name = "${{var.cluster_name}}-node-group" }})
+}}
