bee1f3d2048b04d55e97e2782b173e0d93c20d86
niteshpotu5656 <niteshpotu5656@gmail.com>
2026-05-17 12:33:45 +0530

    feat: add network/ssm/outputs.tf

---
+++ network/ssm/outputs.tf
@@ -0,0 +1,3 @@
+output "ssm_endpoint_id"         { value = aws_vpc_endpoint.ssm.id }
+output "ssmmessages_endpoint_id" { value = aws_vpc_endpoint.ssmmessages.id }
+output "ec2messages_endpoint_id" { value = aws_vpc_endpoint.ec2messages.id }
