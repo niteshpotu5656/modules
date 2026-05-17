dc05a55cda28391d8d873bb60bf9181657fef739
niteshpotu5656 <niteshpotu5656@gmail.com>
2026-05-18 01:35:27 +0530

    Fix: correct HCL syntax in network/ssm/main.tf

--- network/ssm/main.tf
+++ network/ssm/main.tf
@@ -1,31 +1,10 @@
-resource "aws_vpc_endpoint" "ssm" {
-  vpc_id              = var.vpc_id
-  service_name        = "com.amazonaws.${{data.aws_region.current.name}}.ssm"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = var.subnet_ids
-  security_group_ids  = [var.sg_id]
-  private_dns_enabled = true
-  tags = merge(var.tags, {{ Name = "${{var.account_name}}-${{var.environment}}-ssm-endpoint" }})
+resource "aws_ssm_parameter" "main" {
+  for_each = var.parameters
+  name     = "/${var.account_name}/${var.environment}/${each.key}"
+  type     = each.value.sensitive ? "SecureString" : "String"
+  value    = each.value.value
+  key_id   = each.value.sensitive ? var.kms_key_arn : null
+  tags = merge(var.tags, {
+    Name = "${var.account_name}-${var.environment}-${each.key}"
+  })
 }
-
-resource "aws_vpc_endpoint" "ssmmessages" {
-  vpc_id              = var.vpc_id
-  service_name        = "com.amazonaws.${{data.aws_region.current.name}}.ssmmessages"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = var.subnet_ids
-  security_group_ids  = [var.sg_id]
-  private_dns_enabled = true
-  tags = merge(var.tags, {{ Name = "${{var.account_name}}-${{var.environment}}-ssmmessages-endpoint" }})
-}
-
-resource "aws_vpc_endpoint" "ec2messages" {
-  vpc_id              = var.vpc_id
-  service_name        = "com.amazonaws.${{data.aws_region.current.name}}.ec2messages"
-  vpc_endpoint_type   = "Interface"
-  subnet_ids          = var.subnet_ids
-  security_group_ids  = [var.sg_id]
-  private_dns_enabled = true
-  tags = merge(var.tags, {{ Name = "${{var.account_name}}-${{var.environment}}-ec2messages-endpoint" }})
-}
-
-data "aws_region" "current" {{}}
