0e3f4f040900807db5e7516d21f217e1e8f5c907
niteshpotu5656 <niteshpotu5656@gmail.com>
2026-05-18 01:39:07 +0530

    Fix: correct HCL syntax in app/nlb/main.tf

--- app/nlb/main.tf
+++ app/nlb/main.tf
@@ -1,25 +1,37 @@
-resource "aws_lb" "main" {{
-  name               = var.name
-  load_balancer_type = "network"
+resource "aws_lb" "main" {
+  name               = "${var.account_name}-${var.environment}-${var.name}-nlb"
   internal           = var.internal
+  load_balancer_type = "network"
   subnets            = var.subnet_ids
-  tags               = merge(var.tags, {{ Name = var.name }})
-}}
 
-resource "aws_lb_target_group" "main" {{
-  name     = "${{var.name}}-tg"
-  port     = var.target_port
-  protocol = "TCP"
-  vpc_id   = var.vpc_id
-  tags     = merge(var.tags, {{ Name = "${{var.name}}-tg" }})
-}}
+  enable_deletion_protection       = var.environment == "prod" ? true : false
+  enable_cross_zone_load_balancing = true
 
-resource "aws_lb_listener" "main" {{
+  tags = merge(var.tags, { Name = "${var.account_name}-${var.environment}-${var.name}-nlb" })
+}
+
+resource "aws_lb_target_group" "main" {
+  name        = "${var.account_name}-${var.environment}-${var.name}-tg"
+  port        = var.target_port
+  protocol    = "TCP"
+  vpc_id      = var.vpc_id
+  target_type = var.target_type
+
+  health_check {
+    protocol = "TCP"
+    interval = 30
+  }
+
+  tags = merge(var.tags, { Name = "${var.account_name}-${var.environment}-${var.name}-tg" })
+}
+
+resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.arn
-  port              = var.target_port
+  port              = var.listener_port
   protocol          = "TCP"
-  default_action {{
+
+  default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.main.arn
-  }}
-}}
+  }
+}
