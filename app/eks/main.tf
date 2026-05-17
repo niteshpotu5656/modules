35641101ea458bcfd19a05eb5c680d9109662f61
niteshpotu5656 <niteshpotu5656@gmail.com>
2026-05-18 01:44:21 +0530

    Fix: correct HCL syntax in app/eks/main.tf

--- app/eks/main.tf
+++ app/eks/main.tf
@@ -1,67 +1,74 @@
-resource "aws_iam_role" "cluster" {{
-  name = "${{var.cluster_name}}-cluster-role"
-  assume_role_policy = jsonencode({{
+resource "aws_iam_role" "cluster" {
+  name = "${var.cluster_name}-cluster-role"
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{{
+    Statement = [{
       Effect    = "Allow"
-      Principal = {{ Service = "eks.amazonaws.com" }}
+      Principal = { Service = "eks.amazonaws.com" }
       Action    = "sts:AssumeRole"
-    }}]
-  }})
+    }]
+  })
   tags = var.tags
-}}
+}
 
-resource "aws_iam_role_policy_attachment" "cluster_policy" {{
+resource "aws_iam_role_policy_attachment" "cluster_policy" {
   role       = aws_iam_role.cluster.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}}
+}
 
-resource "aws_eks_cluster" "main" {{
+resource "aws_eks_cluster" "main" {
   name     = var.cluster_name
   version  = var.k8s_version
   role_arn = aws_iam_role.cluster.arn
 
-  vpc_config {{
+  vpc_config {
     subnet_ids              = var.subnet_ids
     security_group_ids      = [var.sg_id]
     endpoint_private_access = true
     endpoint_public_access  = false
-  }}
+  }
 
-  encryption_config {{
+  encryption_config {
     resources = ["secrets"]
-    provider {{ key_arn = var.kms_key_arn }}
-  }}
+    provider { key_arn = var.kms_key_arn }
+  }
 
-  tags = merge(var.tags, {{ Name = var.cluster_name }})
+  tags       = merge(var.tags, { Name = var.cluster_name })
   depends_on = [aws_iam_role_policy_attachment.cluster_policy]
-}}
+}
 
-resource "aws_iam_role" "node" {{
-  name = "${{var.cluster_name}}-node-role"
-  assume_role_policy = jsonencode({{
+resource "aws_iam_role" "node" {
+  name = "${var.cluster_name}-node-role"
+  assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{{
+    Statement = [{
       Effect    = "Allow"
-      Principal = {{ Service = "ec2.amazonaws.com" }}
+      Principal = { Service = "ec2.amazonaws.com" }
       Action    = "sts:AssumeRole"
-    }}]
-  }})
+    }]
+  })
   tags = var.tags
-}}
+}
 
-resource "aws_eks_node_group" "main" {{
+resource "aws_iam_role_policy_attachment" "node_policies" {
+  for_each   = toset(["AmazonEKSWorkerNodePolicy", "AmazonEKS_CNI_Policy", "AmazonEC2ContainerRegistryReadOnly"])
+  role       = aws_iam_role.node.name
+  policy_arn = "arn:aws:iam::aws:policy/${each.value}"
+}
+
+resource "aws_eks_node_group" "main" {
   cluster_name    = aws_eks_cluster.main.name
-  node_group_name = "${{var.cluster_name}}-node-group"
+  node_group_name = "${var.cluster_name}-node-group"
   node_role_arn   = aws_iam_role.node.arn
   subnet_ids      = var.subnet_ids
   instance_types  = [var.node_instance_type]
 
-  scaling_config {{
+  scaling_config {
     desired_size = var.node_desired
     min_size     = var.node_min
     max_size     = var.node_max
-  }}
+  }
 
-  tags = merge(var.tags, {{ Name = "${{var.cluster_name}}-node-group" }})
-}}
+  tags       = merge(var.tags, { Name = "${var.cluster_name}-node-group" })
+  depends_on = [aws_iam_role_policy_attachment.node_policies]
+}
