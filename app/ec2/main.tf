e11741052c31cbf250a12ea4abeeba9cca0f1fb8
niteshpotu5656 <niteshpotu5656@gmail.com>
2026-05-18 01:39:04 +0530

    Fix: correct HCL syntax in app/ec2/main.tf

--- app/ec2/main.tf
+++ app/ec2/main.tf
@@ -1,27 +1,33 @@
-resource "aws_iam_instance_profile" "main" {
-  name = "${{var.name}}-profile"
-  role = var.iam_role_name
-  tags = var.tags
+data "aws_ami" "al2" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
 }
 
 resource "aws_instance" "main" {
-  ami                    = var.ami_id
+  ami                    = var.ami_id != "" ? var.ami_id : data.aws_ami.al2.id
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.sg_ids
-  iam_instance_profile   = aws_iam_instance_profile.main.name
-  user_data              = var.user_data
-  monitoring             = true
+  iam_instance_profile   = var.instance_profile
+  key_name               = var.key_name
 
-  root_block_device {{
+  root_block_device {
+    volume_type = "gp3"
+    volume_size = var.root_volume_size
     encrypted   = true
     kms_key_id  = var.kms_key_arn
-    volume_type = "gp3"
-  }}
+  }
 
-  metadata_options {{
-    http_tokens = "required"
-  }}
+  metadata_options {
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
 
-  tags = merge(var.tags, {{ Name = var.name }})
+  tags = merge(var.tags, {
+    Name = "${var.account_name}-${var.environment}-${var.instance_name}"
+  })
 }
