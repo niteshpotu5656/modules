1b47733a93726d94e7a6e88ad835a14411e88b26
niteshpotu5656 <niteshpotu5656@gmail.com>
2026-05-17 12:34:54 +0530

    feat: add app/secrets-manager/main.tf

---
+++ app/secrets-manager/main.tf
@@ -0,0 +1,12 @@
+resource "aws_secretsmanager_secret" "main" {{
+  name                    = "${{var.account_name}}/${{var.environment}}/${{var.name}}"
+  description             = var.description
+  kms_key_id              = var.kms_key_arn
+  recovery_window_in_days = var.environment == "prod" ? 30 : 7
+  tags                    = merge(var.tags, {{ Name = var.name }})
+}}
+
+resource "aws_secretsmanager_secret_version" "main" {{
+  secret_id     = aws_secretsmanager_secret.main.id
+  secret_string = var.secret_value
+}}
