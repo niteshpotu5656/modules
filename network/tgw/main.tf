57ce3f96bb859d90e84526ad960ada065f484f06
niteshpotu5656 <niteshpotu5656@gmail.com>
2026-05-18 01:34:57 +0530

    Fix: correct HCL syntax in network/tgw/main.tf

--- network/tgw/main.tf
+++ network/tgw/main.tf
@@ -2,9 +2,20 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "main" {
   transit_gateway_id = var.tgw_id
   vpc_id             = var.vpc_id
   subnet_ids         = var.subnet_ids
-  tags               = merge(var.tags, {{
-    Name = "${{var.account_name}}-${{var.environment}}-tgw-attachment"
-  }})
+  tags = merge(var.tags, {
+    Name = "${var.account_name}-${var.environment}-tgw-attachment"
+  })
+}
+
+data "aws_ec2_transit_gateway_route_table" "main" {
+  filter {
+    name   = "transit-gateway-id"
+    values = [var.tgw_id]
+  }
+  filter {
+    name   = "default-association-route-table"
+    values = ["true"]
+  }
 }
 
 resource "aws_ec2_transit_gateway_route" "static" {
@@ -13,14 +24,3 @@ resource "aws_ec2_transit_gateway_route" "static" {
   destination_cidr_block         = var.static_routes[count.index]
   transit_gateway_attachment_id  = aws_ec2_transit_gateway_vpc_attachment.main.id
 }
-
-data "aws_ec2_transit_gateway_route_table" "main" {{
-  filter {{
-    name   = "transit-gateway-id"
-    values = [var.tgw_id]
-  }}
-  filter {{
-    name   = "default-association-route-table"
-    values = ["true"]
-  }}
-}}
