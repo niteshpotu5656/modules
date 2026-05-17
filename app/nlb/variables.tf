ff6dbbc36da651e62c8a32380b370e47ec828827
niteshpotu5656 <niteshpotu5656@gmail.com>
2026-05-17 12:34:22 +0530

    feat: add app/nlb/variables.tf

---
+++ app/nlb/variables.tf
@@ -0,0 +1,26 @@
+variable "name"        { description = "NLB name" }
+variable "internal"    { default = true }
+variable "subnet_ids"  { type = list(string) }
+variable "vpc_id"      { description = "VPC ID" }
+variable "target_port" { default = 443 }
+variable "account_name" { description = "AWS account name" }
+variable "environment"  { description = "Environment" }
+
+variable "tags" {
+  description = "All 24 mandatory tags that must be applied to every resource."
+  type        = map(string)
+  validation {
+    condition = alltrue([
+      for tag in [
+        "AccountId", "AccountName", "AppId", "ApplicationType",
+        "Environment", "Manager", "CostCenter", "BusinessUnit",
+        "Department", "Project", "Owner", "CreatedBy", "CreatedDate",
+        "LastModifiedBy", "LastModifiedDate", "Compliance",
+        "DataClassification", "BackupPolicy", "MonitoringLevel",
+        "PatchGroup", "SupportTeam", "Region", "Terraform",
+        "TerraformModuleVersion"
+      ] : contains(keys(var.tags), tag)
+    ])
+    error_message = "All 24 mandatory tags must be provided."
+  }
+}
