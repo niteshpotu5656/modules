d10391f784fed5bb740feeeb62b1739b7459d954
niteshpotu5656 <niteshpotu5656@gmail.com>
2026-05-18 01:44:50 +0530

    Fix: correct HCL syntax in app/sqs/main.tf

--- app/sqs/main.tf
+++ app/sqs/main.tf
@@ -1,18 +1,21 @@
-resource "aws_sqs_queue" "dlq" {{
-  count                     = var.dlq_enabled ? 1 : 0
-  name                      = "${{var.name}}-dlq${{var.fifo ? ".fifo" : ""}}"
-  fifo_queue                = var.fifo
+resource "aws_sqs_queue" "dlq" {
+  count                     = var.create_dlq ? 1 : 0
+  name                      = "${var.account_name}-${var.environment}-${var.queue_name}-dlq"
   kms_master_key_id         = var.kms_key_arn
-  tags                      = merge(var.tags, {{ Name = "${{var.name}}-dlq" }})
-}}
+  message_retention_seconds = 1209600
+  tags                      = merge(var.tags, { Name = "${var.account_name}-${var.environment}-${var.queue_name}-dlq" })
+}
 
-resource "aws_sqs_queue" "main" {{
-  name                      = "${{var.name}}${{var.fifo ? ".fifo" : ""}}"
-  fifo_queue                = var.fifo
-  kms_master_key_id         = var.kms_key_arn
-  redrive_policy            = var.dlq_enabled ? jsonencode({{
+resource "aws_sqs_queue" "main" {
+  name                       = "${var.account_name}-${var.environment}-${var.queue_name}"
+  visibility_timeout_seconds = var.visibility_timeout
+  message_retention_seconds  = var.message_retention
+  kms_master_key_id          = var.kms_key_arn
+
+  redrive_policy = var.create_dlq ? jsonencode({
     deadLetterTargetArn = aws_sqs_queue.dlq[0].arn
     maxReceiveCount     = var.max_receive_count
-  }}) : null
-  tags = merge(var.tags, {{ Name = var.name }})
-}}
+  }) : null
+
+  tags = merge(var.tags, { Name = "${var.account_name}-${var.environment}-${var.queue_name}" })
+}
