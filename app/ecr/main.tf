c850c0e2d74a862293a854235efb93877d495e77
niteshpotu5656 <niteshpotu5656@gmail.com>
2026-05-18 01:44:20 +0530

    Fix: correct HCL syntax in app/ecr/main.tf

--- app/ecr/main.tf
+++ app/ecr/main.tf
@@ -1,28 +1,47 @@
-resource "aws_ecr_repository" "main" {{
-  name                 = var.name
+data "aws_caller_identity" "current" {}
+
+resource "aws_ecr_repository" "main" {
+  name                 = "${var.account_name}/${var.environment}/${var.repo_name}"
   image_tag_mutability = "IMMUTABLE"
-  image_scanning_configuration {{
+
+  image_scanning_configuration {
     scan_on_push = true
-  }}
-  encryption_configuration {{
+  }
+
+  encryption_configuration {
     encryption_type = "KMS"
     kms_key         = var.kms_key_arn
-  }}
-  tags = merge(var.tags, {{ Name = var.name }})
-}}
+  }
 
-resource "aws_ecr_lifecycle_policy" "main" {{
+  tags = merge(var.tags, { Name = "${var.account_name}-${var.environment}-${var.repo_name}" })
+}
+
+resource "aws_ecr_lifecycle_policy" "main" {
   repository = aws_ecr_repository.main.name
-  policy = jsonencode({{
-    rules = [{{
+  policy = jsonencode({
+    rules = [{
       rulePriority = 1
-      description  = "Keep last ${{var.image_count_limit}} images"
-      selection = {{
-        tagStatus   = "any"
-        countType   = "imageCountMoreThan"
-        countNumber = var.image_count_limit
-      }}
-      action = {{ type = "expire" }}
-    }}]
-  }})
-}}
+      description  = "Keep last 10 production images"
+      selection = {
+        tagStatus     = "tagged"
+        tagPrefixList = ["v"]
+        countType     = "imageCountMoreThan"
+        countNumber   = 10
+      }
+      action = { type = "expire" }
+    }]
+  })
+}
+
+resource "aws_ecr_repository_policy" "main" {
+  repository = aws_ecr_repository.main.name
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Sid    = "AllowAccountPull"
+      Effect = "Allow"
+      Principal = { AWS = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:root" }
+      Action = ["ecr:GetDownloadUrlForLayer", "ecr:BatchGetImage", "ecr:BatchCheckLayerAvailability"]
+    }]
+  })
+}
