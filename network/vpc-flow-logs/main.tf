f23ad897e24baad69992b4cb4bb2627880b40f72
niteshpotu5656 <niteshpotu5656@gmail.com>
2026-05-18 01:35:28 +0530

    Fix: correct HCL syntax in network/vpc-flow-logs/main.tf

--- network/vpc-flow-logs/main.tf
+++ network/vpc-flow-logs/main.tf
@@ -1,34 +1,43 @@
-resource "aws_iam_role" "flow_log" {
-  name = "${{var.account_name}}-${{var.environment}}-flow-log-role"
-  assume_role_policy = jsonencode({{
-    Version = "2012-10-17"
-    Statement = [{{
-      Effect    = "Allow"
-      Principal = {{ Service = "vpc-flow-logs.amazonaws.com" }}
-      Action    = "sts:AssumeRole"
-    }}]
-  }})
-  tags = var.tags
+resource "aws_cloudwatch_log_group" "flow_logs" {
+  name              = "/aws/vpc-flow-logs/${var.account_name}-${var.environment}"
+  retention_in_days = var.environment == "prod" ? 365 : 90
+  kms_key_id        = var.kms_key_arn
+  tags              = merge(var.tags, { Name = "${var.account_name}-${var.environment}-flow-logs" })
 }
 
-resource "aws_iam_role_policy" "flow_log" {
-  name   = "flow-log-s3-policy"
-  role   = aws_iam_role.flow_log.id
-  policy = jsonencode({{
+data "aws_iam_policy_document" "flow_logs_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["vpc-flow-logs.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "flow_logs" {
+  name               = "${var.account_name}-${var.environment}-flow-logs-role"
+  assume_role_policy = data.aws_iam_policy_document.flow_logs_assume.json
+  tags               = var.tags
+}
+
+resource "aws_iam_role_policy" "flow_logs" {
+  name = "flow-logs-policy"
+  role = aws_iam_role.flow_logs.id
+  policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{{
+    Statement = [{
       Effect   = "Allow"
-      Action   = ["s3:PutObject"]
-      Resource = "${{var.log_bucket_arn}}/vpc-flow-logs/*"
-    }}]
-  }})
+      Action   = ["logs:CreateLogStream", "logs:PutLogEvents", "logs:DescribeLogGroups", "logs:DescribeLogStreams"]
+      Resource = "${aws_cloudwatch_log_group.flow_logs.arn}:*"
+    }]
+  })
 }
 
 resource "aws_flow_log" "main" {
-  vpc_id               = var.vpc_id
-  traffic_type         = "ALL"
-  log_destination_type = "s3"
-  log_destination      = "${{var.log_bucket_arn}}/vpc-flow-logs/"
-  iam_role_arn         = aws_iam_role.flow_log.arn
-  tags                 = merge(var.tags, {{ Name = "${{var.account_name}}-${{var.environment}}-flow-log" }})
+  iam_role_arn    = aws_iam_role.flow_logs.arn
+  log_destination = aws_cloudwatch_log_group.flow_logs.arn
+  traffic_type    = "ALL"
+  vpc_id          = var.vpc_id
+  tags = merge(var.tags, { Name = "${var.account_name}-${var.environment}-flow-log" })
 }
