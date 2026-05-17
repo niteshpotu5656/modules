9df3d02140b83272f6b718701cd1e139fc50e168
niteshpotu5656 <niteshpotu5656@gmail.com>
2026-05-18 01:44:49 +0530

    Fix: correct HCL syntax in app/sns/main.tf

--- app/sns/main.tf
+++ app/sns/main.tf
@@ -1,5 +1,12 @@
-resource "aws_sns_topic" "main" {{
-  name              = var.name
+resource "aws_sns_topic" "main" {
+  name              = "${var.account_name}-${var.environment}-${var.topic_name}"
   kms_master_key_id = var.kms_key_arn
-  tags              = merge(var.tags, {{ Name = var.name }})
-}}
+  tags              = merge(var.tags, { Name = "${var.account_name}-${var.environment}-${var.topic_name}" })
+}
+
+resource "aws_sns_topic_subscription" "main" {
+  for_each  = { for s in var.subscriptions : s.endpoint => s }
+  topic_arn = aws_sns_topic.main.arn
+  protocol  = each.value.protocol
+  endpoint  = each.value.endpoint
+}
